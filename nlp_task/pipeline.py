79ffa4262b832c96ec2c44f9eef1b446f6f2c11f
charles <CHZarles@github.com>
2024-11-16 10:27:48 +0800

    update

--- nlp_task/pipeline.py
+++ nlp_task/pipeline.py
@@ -1,11 +1,11 @@
 import json
 
 import datasets
+from torch.utils.tensorboard import SummaryWriter
 
 from bleu_metrics import BLEU
 
 model_checkpoint = "uer/t5-base-chinese-cluecorpussmall"
-
 
 # 1. 加载数据集
 train_path = "./DuReaderQG/train.json"
@@ -49,6 +49,17 @@ import numpy as np
 bleu_evaluators = [BLEU(n_size=i + 1) for i in range(4)]
 
 
+writer = SummaryWriter("training_logs")
+
+
+train_dataset_size = len(tokenized_datasets["train"])
+batch_size = 12
+step_each_epoch = train_dataset_size // batch_size
+echo_times = 0
+
+
+# tensorbard 显示问题
+# https://github.com/tensorflow/tensorboard/issues/6808
 def compute_metrics(pred):
     print("call metrics")
     predictions, labels = pred
@@ -63,13 +74,22 @@ def compute_metrics(pred):
         "bleu3": bleu_evaluators[2].compute(),
         "bleu4": bleu_evaluators[3].compute(),
     }
+    global echo_times
+    global step_each_epoch
+    echo_times += 1
+    print("echo_times * step_each_epoch", echo_times * step_each_epoch)
+    for i in range(4):
+        writer.add_scalar(
+            "eval/bleu-size-{}".format(i + 1),
+            result[f"bleu{i + 1}"],
+            echo_times * step_each_epoch,
+        )
     return result
 
 
 # 5. 设置训练参数
 from transformers import Seq2SeqTrainingArguments
 
-batch_size = 16
 train_args = Seq2SeqTrainingArguments(
     "QuestionAnswer",
     evaluation_strategy="epoch",
@@ -107,8 +127,8 @@ from transformers import pipeline
 
 pipe = pipeline("text2text-generation", model=model, tokenizer=tokenizer, device=0)
 
-question = "治疗宫颈糜烂的最佳时间"
-context = "专家指出，宫颈糜烂治疗时间应选在月经干净后3-7日，因为治疗之后宫颈有一定的创面，如赶上月经期易发生感染。因此患者应在月经干净后3天尽快来医院治疗。同时应该注意，术前3天禁同房，有生殖道急性炎症者应治好后才可进行。"
+question = "广州英语培训哪家好"
+context = "广州学英语,我认为一定要找对学校,否则花了钱还浪费了时间。广州的英语培训学校,大多是夜校性质,走读那种,通常是来也匆匆,去也匆匆,没有多少停留操练或静心读书的机会。遇到滂沱大雨或交通拥堵,缺课旷课是常有的事。 鉴于上述情况,我建议你报读全封闭式英语培训学校。广州有一家是专门做封闭式英语口语强化训练的学校,叫广州东方英文书院。这家培训机构从事全封闭英语教学17年,很少卖广告,从来就不搞什么招生噱头,吸引眼球;而是注重口碑宣传,因为口碑是最好的广告;而口碑是靠质量去打造。该书院位于山清水秀的五A级白云山里面,环境一流,中国唯一一家能将培训学校办在五A级风景区里面的培训学校。我曾经有同事去读过,觉得学英语确实需要一个安静的环境,这样才能做到六根清净潜心读书,否则匆匆而来、急急而去,天天走读,很难有一个很好的连贯性,进步显然没有全封闭式培训那么明显。要在广州学英语,不如索性封闭起来,将空余时间用到尽,也许能让你学有所成。"
 input_str = "问题：{question}{sep_token}原文：{context}".format(
     question=question, context=context, sep_token=tokenizer.sep_token
 )
