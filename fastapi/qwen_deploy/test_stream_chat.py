7c2827e796ce71fe4596b3cf1c49cd630bca8709
charles <CHZarles@github.com>
2025-01-04 01:29:30 +0800

    update

--- fastapi/qwen_deploy/test_stream_chat.py
+++ fastapi/qwen_deploy/test_stream_chat.py
@@ -1,26 +1,25 @@
 from threading import Thread
 
-from transformers import (AutoModelForCausalLM, AutoTokenizer,
-                          GenerationConfig, TextIteratorStreamer)
-
-device = "cuda" # 加载模型的设备
-model_path="./qwen/Qwen1.5-7B-Chat" # 模型路径
-generation_config = GenerationConfig.from_pretrained(model_path)
-model = AutoModelForCausalLM.from_pretrained(
-model_path,
-device_map="auto"
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    GenerationConfig,
+    TextIteratorStreamer,
 )
+
+device = "cuda"  # 加载模型的设备
+model_path = "./qwen/Qwen1.5-7B-Chat"  # 模型路径
+generation_config = GenerationConfig.from_pretrained(model_path)
+model = AutoModelForCausalLM.from_pretrained(model_path, device_map="auto")
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 
 prompt = "请介绍一下基于人类反馈的强化学习HFRL方法"
 messages = [
-{"role": "system", "content": "You are a helpful assistant."},
-{"role": "user", "content": prompt}
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": prompt},
 ]
 text = tokenizer.apply_chat_template(
-messages,
-tokenize=False,
-add_generation_prompt=True
+    messages, tokenize=False, add_generation_prompt=True
 )
 model_inputs = tokenizer([text], return_tensors="pt").to(device)
 
@@ -34,5 +33,5 @@ generated_text = ""
 for new_text in streamer:
     generated_text += new_text
 # yield generated_text
-print('####################################')
-print(generated_text)`
+print("####################################")
+print(generated_text)
