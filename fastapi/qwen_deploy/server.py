df57ee0b70cf0a1955f50581b60c345f86122c1c
charles <CHZarles@github.com>
2025-01-03 10:06:39 +0800

    delete qwen_chat

--- fastapi/qwen_deploy/server.py
+++ fastapi/qwen_deploy/server.py
@@ -10,6 +10,7 @@ from transformers.generation import GenerationConfig
 
 from message_type import (
     ChatCompletionRequest,
+    ChatCompletionResponse,
     ChatCompletionResponseChoice,
     ChatCompletionResponseStreamChoice,
     ChatMessage,
@@ -17,13 +18,17 @@ from message_type import (
     DeltaMessage,
     ModelList,
     PromptTokensDetails,
+    Usage,
 )
+from qwen_api import Qwen
 
 qwen_model = None
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    global qwen_model
+    qwen_model = Qwen(model_path="./Qwen/Qwen1.5-7B-Chat", device="cuda")
     yield
     if torch.cuda.is_available():
         torch.cuda.empty_cache()
@@ -59,7 +64,64 @@ async def list_models():
 
 @app.get("/v1/chat/completions", response_model=ChatCompletionResponse)
 async def create_chat_completion(request: ChatCompletionRequest):
-    pass
+    if request.messages[-1].role != "user":
+        raise HTTPException(status_code=400, detail="Invalid request")
+
+    # TODO: implement the following logic
+    if request.stream:
+        HTTPException(status_code=400, detail="stream not supported")
+
+    """
+    # construct messages template
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": prompt},
+    ]
+    """
+    messages = []
+    for message in request.messages:
+        messages.append({"role": message.role, "content": message.content})
+
+    # call qwen generate
+    response = qwen_model.generate_response(messages)
+    # make
+    print(response)
+    content = response["generated_text"]
+    choice_data = ChatCompletionResponseChoice(
+        index=0,
+        message=ChatMessage(role="assistant", content=content),
+        finish_reason="stop",
+    )
+    usage_data = Usage(
+        completion_tokens=response["completion_tokens"],
+        prompt_tokens=response["prompt_tokens"],
+        total_tokens=response["prompt_tokens"] + response["completion_tokens"],
+    )
+    print(usage_data)
+    res = ChatCompletionResponse(
+        model=request.model,
+        choices=[choice_data],
+        usage=usage_data,
+        object="chat.completion",
+    )
+    return res
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    """
+    input: JSON String of {"query": "", "history": []}
+    output: JSON String of {"response": "", "history": [], "status": 200}
+        status 200 stand for response ended, else not
+    """
+    # TODO: implement this
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await websocket.send_text(f"At {time.time()} Message text was: {data}")
+    except WebSocketDisconnect:
+        print("Client disconnected")
 
 
 if __name__ == "__main__":
