0070d7c85d194fcb92825b124817007f15eca6d4
charles <xxxxx>
2025-01-04 02:14:31 +0800

    update

--- fastapi/qwen_deploy/test_stream_chat.py
+++ fastapi/qwen_deploy/test_stream_chat.py
@@ -13,7 +13,7 @@ generation_config = GenerationConfig.from_pretrained(model_path)
 model = AutoModelForCausalLM.from_pretrained(model_path, device_map="auto")
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 
-prompt = "请介绍一下基于人类反馈的强化学习HFRL方法"
+prompt = "什么是中国红"
 messages = [
     {"role": "system", "content": "You are a helpful assistant."},
     {"role": "user", "content": prompt},
@@ -22,16 +22,18 @@ text = tokenizer.apply_chat_template(
     messages, tokenize=False, add_generation_prompt=True
 )
 model_inputs = tokenizer([text], return_tensors="pt").to(device)
-
+print("call TextIteratorStreamer")
 streamer = TextIteratorStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
-generation_kwargs = dict(model_inputs, streamer=streamer, max_new_tokens=8192)
+generation_kwargs = dict(model_inputs, streamer=streamer, max_new_tokens=2048)
 
+print("call Thread")
 thread = Thread(target=model.generate, kwargs=generation_kwargs)
 thread.start()
 
 generated_text = ""
 for new_text in streamer:
     generated_text += new_text
+    print(new_text)
 # yield generated_text
 print("####################################")
 print(generated_text)
