2041b509ff7ba5788aa428aa07dfc2a50bd274f8
charles <CHZarles@github.com>
2025-01-04 14:21:10 +0800

    update

--- fastapi/qwen_deploy/qwen_client.py
+++ fastapi/qwen_deploy/qwen_client.py
@@ -8,6 +8,7 @@ from message_type import *
 MODEL_UID = "qwen1.5-chat-7b"
 URL = "http://localhost:8000/v1/chat/completions"
 prompt = input("Enter your prompt: ")
+stream = False
 payload = {
     "model": MODEL_UID,
     "n": 1,
@@ -15,12 +16,23 @@ payload = {
     "top_p": 1.0,
     "messages": [{"role": "user", "content": prompt}],
     "max_tokens": 8192,
+    "stream:": stream,
 }
 
-# requests
 response = requests.post(URL, json=payload)
-if response.status_code == 200:
-    completion = response.json()
-    print("Response:", completion)
+# requests
+if not stream:
+    if response.status_code == 200:
+        completion = response.json()
+        print("Response:", completion)
+    else:
+        print("Failed to get response:", response.status_code, response.text)
 else:
-    print("Failed to get response:", response.status_code, response.text)
+    # handle steam response
+    if response.status_code == 200:
+        for line in response.iter_lines():
+            if line:
+                decoded_line = line.decode("utf-8")
+                print("Streamed Response:", decoded_line)
+    else:
+        print("Failed to get response:", response.status_code, response.text)
