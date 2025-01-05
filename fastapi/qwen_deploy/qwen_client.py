53123ded1f363d7c032313c3ce16418117762f18
charles <CHZarles@github.com>
2025-01-05 12:58:17 +0800

    update qwen client

--- fastapi/qwen_deploy/qwen_client.py
+++ fastapi/qwen_deploy/qwen_client.py
@@ -1,55 +1,123 @@
+import asyncio
+import json
+import os
+import re
+from typing import List
+
 import requests
+import websockets
 
 from message_type import *
 
 # ChatCompletionRequest
 # url: localhost:8000/v1/chat/completions
 
-MODEL_UID = "qwen1.5-chat-7b"
-URL = "http://localhost:8000/v1/chat/completions"
-prompt = input("Enter your prompt: ")
-stream = False
-payload = {
-    "model": MODEL_UID,
-    "n": 1,
-    "temperature": 0,
-    "top_p": 1.0,
-    "messages": [{"role": "user", "content": prompt}],
-    "max_tokens": 8192,
-    "stream:": stream,
-}
 
-response = requests.post(URL, json=payload)
-# requests
-if not stream:
-    if response.status_code == 200:
-        completion = response.json()
-        print("Response:", completion)
+# [{"role": "user", "content": prompt}]
+def request_http(messages: List[Dict[str, str]], stream: bool = False):
+    MODEL_UID = "qwen1.5-chat-7b"
+    URL = "http://localhost:8000/v1/chat/completions"
+    payload = {
+        "model": MODEL_UID,
+        "n": 1,
+        "temperature": 0,
+        "top_p": 1.0,
+        "messages": messages,
+        "max_tokens": 8192,
+        "stream:": stream,
+    }
+
+    response = requests.post(URL, json=payload)
+    # requests
+    if not stream:
+        if response.status_code == 200:
+            completion = response.json()
+            print("Response:", completion)
+        else:
+            print("Failed to get response:", response.status_code, response.text)
     else:
-        print("Failed to get response:", response.status_code, response.text)
-else:
-    # NOTE: 实际测试中这个代码还是会一次把结果打印回来,我期望的效果是下面这种
-    """
-    curl -X POST  http://localhost:8000/v1/chat/completions   -H "Content-Type: application/json" `
-    -d '{
-        "model": "qwen1.5-7b-chat",
-        "messages": [
-          {
-            "role": "system",
-            "content": "You are a helpful assistant."
-          },
-          {
-            "role": "user",
-            "content": "Hello!"
-          }
-        ],
-        "stream": true
-      }'
-    """
-    if response.status_code == 200:
-        # handle steam response
-        for chunk in response.iter_content(chunk_size=1024):
-            # 处理响应内容
-            print(chunk.decode("utf-8"))
-    else:
-        print("Failed to get response:", response.status_code, response.text)
+        # NOTE: 实际测试中这个代码还是会一次把结果打印回来,我期望的效果是下面这种
+        """
+        curl -X POST  http://localhost:8000/v1/chat/completions   -H "Content-Type: application/json" `
+        -d '{
+            "model": "qwen1.5-7b-chat",
+            "messages": [
+              {
+                "role": "system",
+                "content": "You are a helpful assistant."
+              },
+              {
+                "role": "user",
+                "content": "Hello!"
+              }
+            ],
+            "stream": true
+          }'
+        """
+        if response.status_code == 200:
+            # handle steam response
+            for chunk in response.iter_content(chunk_size=1024):
+                # 处理响应内容
+                print(chunk.decode("utf-8"))
+        else:
+            print("Failed to get response:", response.status_code, response.text)
+
+
+def request_websocket(query::str, history: List[Tuple[str,str]]):
+    # wscat ws://localhost:8000/chat/completions/ws
+    uri = "ws://localhost:8000/chat/completions/ws"
+    async with websockets.connect(uri) as websocket:
+        # Prepare the input JSON string
+        input_data = {"query": query, "history": history}
+        await websocket.send(json.dumps(input_data))
+
+        # Receive the output JSON string
+        response = await websocket.recv()
+        output_data = json.loads(response)
+
+        # Print the response
+        print(f"Response: {output_data['response']}")
+        print(f"History: {output_data['history']}")
+        print(f"Status: {output_data['status']}")
+
+
+if __main__ == "__main__":
+    #construct a messages with history
+    messages = [
+            {
+                "role":"system"
+                "content": "You are a helpful assistant."
+            },
+            {
+                "role": "user",
+                "content": "一拳超人埼玉的朋友是谁"
+            },
+            {
+                "role": "assistant",
+                "content": "在《一拳超人》这部漫画中，埼玉（Saitama）最常被提及的朋友是杰诺斯（Genos）。杰诺斯是一位改造人，他在埼玉的帮助下从爆炸中生还，之后便成为了埼玉的弟子和室友。杰诺斯对埼玉非常尊敬，经常试图学习埼玉的“秘诀”，尽管埼玉本人认为自己只是通过普通的锻炼变得强大。"
+            },
+            {
+                "role": "user",
+                "content": "他们两个谁的等级高" 
+                }
+            ]
+
+    # request the HTTP using the requests library
+    request_http(messages, stream=False)
+    request_http(messages, stream=True)
+
+    # request the HTTP using curl
+    playload = {
+            "model": "qwen1.5-7b-chat",
+            "messages": messages,
+            "stream": False
+            }
+    curl_command = f"curl -X POST http://localhost:8000/v1/chat/completions -H 'Content-Type: application/json' -d '{json.dumps(playload)}'"
+    os.system(curl_command)
+    playload["stream"]=True
+    os.system(curl_command)
+
+    # request the websocket
+    query = messages[-1]["content"]
+    history = [(m["role"], m["content"]) for m in messages[:-1]]
+    asyncio.run(request_websocket(query, history))
